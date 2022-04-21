a9d92dec3c20d0d07d06053b4bfa583ab9b03d5f
Maciej Kasprzyk <maciej.kasprzyk@contractors.roche.com>
2022-04-21 14:11:01 +0000

    Working PoC

--- burned-calories/src/main/java/cc/leniwie/burned_calories/BurnedCaloriesTransformer.java
+++ burned-calories/src/main/java/cc/leniwie/burned_calories/BurnedCaloriesTransformer.java
@@ -20,6 +20,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 
+import io.hammerhead.sdk.v0.RideState;
 import io.hammerhead.sdk.v0.SdkContext;
 import io.hammerhead.sdk.v0.datatype.Dependency;
 import io.hammerhead.sdk.v0.datatype.transformer.SdkTransformer;
@@ -30,11 +31,18 @@ class BurnedCaloriesTransformer extends SdkTransformer {
     }
 
     @Override
+    public boolean onStart() {
+        SdkContext sdkContext = getContext();
+        return sdkContext.getKeyValueStore().putDouble("burnedCalories", 0.0);
+    }
+
+    @Override
     public double onDependencyChange(long timeStamp, @NotNull Map<Dependency, Double> dependencies) {
         Double heartRate = dependencies.get(Dependency.HEART_RATE);
-        if (heartRate == null || heartRate == MISSING_VALUE) {
+        if (heartRate == null || heartRate == MISSING_VALUE || getRideState() != RideState.RECORDING) {
             return MISSING_VALUE;
         }
-        return heartRate;
+        Double burnedCalories = getContext().getKeyValueStore().getDouble("burnedCalories");
+        return burnedCalories;
     }
 }
