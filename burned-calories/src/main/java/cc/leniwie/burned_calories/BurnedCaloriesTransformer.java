a386ae7686d9765819513fcd26cdf53819cabf9c
Maciej Kasprzyk <maciej.kasprzyk@contractors.roche.com>
2022-04-22 20:31:15 +0000

    Revert avg changes

--- burned-calories/src/main/java/cc/leniwie/burned_calories/BurnedCaloriesTransformer.java
+++ burned-calories/src/main/java/cc/leniwie/burned_calories/BurnedCaloriesTransformer.java
@@ -18,8 +18,6 @@ package cc.leniwie.burned_calories;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.OptionalDouble;
-import java.util.ArrayList;
 import java.util.Map;
 import java.sql.Timestamp;
 
@@ -107,9 +105,8 @@ class BurnedCaloriesTransformer extends SdkTransformer {
     private Double weight = kvStore.getDouble(sdkContext.getResources().getString(R.string.weightKey));
     private Double age = kvStore.getDouble(sdkContext.getResources().getString(R.string.ageKey));
 
-    ArrayList<Double> heartRates = new ArrayList();
-    private Double previousTimestamp = (double) System.nanoTime();
-    private Double probeSeconds = 0.0;
+    private double previousTimestamp = (double) System.nanoTime();
+    private double probeSeconds = 0.0;
 
     public BurnedCaloriesTransformer(@NotNull SdkContext context) {
         super(context);
@@ -132,10 +129,6 @@ class BurnedCaloriesTransformer extends SdkTransformer {
 
     @Override
     public double onDependencyChange(long timeStamp, @NotNull Map<Dependency, Double> dependencies) {
-        Double heartRate = dependencies.get(Dependency.HEART_RATE);
-        if (heartRate == null || heartRate == MISSING_VALUE) {
-            return MISSING_VALUE;
-        }
 
         double currentTimestamp = (double) System.nanoTime();
         double updateDrift = (currentTimestamp - previousTimestamp) / 1000000000;
@@ -143,28 +136,25 @@ class BurnedCaloriesTransformer extends SdkTransformer {
 
         if (probeSeconds < 1.0) {
             probeSeconds += updateDrift;
-            heartRates.add(heartRate);
             return MISSING_VALUE;
+        } else {
+            kvStore.putDouble("updateDrift", probeSeconds);
+            probeSeconds = 0.0;
         }
 
-        kvStore.putDouble("updateDrift", probeSeconds);
-        probeSeconds = 0.0;
-
-        Double avgHeartRate = heartRates
-            .stream()
-            .mapToDouble(a -> a)
-            .average()
-            .orElse(0.0);
-        heartRates.clear();
+        Double heartRate = dependencies.get(Dependency.HEART_RATE);
+        if (heartRate == null || heartRate == MISSING_VALUE) {
+            return MISSING_VALUE;
+        }
 
         Double burnedCalories = kvStore.getDouble(this.burnedCaloriesKey);
 
         switch(gender) {
         case "Male":
-            burnedCalories += new BurnedCaloriesEquationMale(avgHeartRate, weight, age, probeSeconds).calculate();
+            burnedCalories += new BurnedCaloriesEquationMale(heartRate, weight, age, probeSeconds).calculate();
             break;
         case "Female":
-            burnedCalories += new BurnedCaloriesEquationFemale(avgHeartRate, weight, age, probeSeconds).calculate();
+            burnedCalories += new BurnedCaloriesEquationFemale(heartRate, weight, age, probeSeconds).calculate();
             break;
         }
 
